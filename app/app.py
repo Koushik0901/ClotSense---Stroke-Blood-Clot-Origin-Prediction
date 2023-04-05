c98166fa73958665bfb32cc5ded98301993f5143
Koushik <koushik.nov01@gmail.com>
2023-04-05 16:50:47 +0530

    fixed for demo

--- app/app.py
+++ app/app.py
@@ -4,7 +4,6 @@ import pymysql
 from engine import InferenceEngine
 from flask import Flask, jsonify, redirect, render_template, request, url_for
 from flaskext.mysql import MySQL
-from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config["SAVE_DIR"] = "data"
@@ -43,10 +42,10 @@ def login():
         user = request.form["email"]
         password = request.form["password"]
         cursor.execute(
-            "SELECT password FROM users WHERE email = %s", (user)
+            "SELECT password FROM users WHERE email = %s AND password = %s", (user, password)
         )
         db_password = cursor.fetchone()
-        if len(db_password) != 0 and check_password_hash(db_password, password):
+        if len(db_password) != 0:
             return redirect(url_for("predict"))
         else:
             login_error = "Invalid email or password"
@@ -60,7 +59,6 @@ def register():
     if request.method == "POST":
         user = request.form["email"]
         password = request.form["password"]
-        hashed_password = generate_password_hash(password)
         cursor.execute("SELECT * FROM users WHERE email = %s", (user,))
         results = cursor.fetchall()
         if len(results) != 0:
@@ -68,11 +66,11 @@ def register():
             return render_template("index.html", register_error=register_error)
         else:
             cursor.execute(
-                f"INSERT INTO users (email, password) VALUES ('{user}', '{hashed_password}')"
+                f"INSERT INTO users (email, password) VALUES ('{user}', '{password}')"
             )
             conn.commit()
             print(cursor.rowcount, "record inserted.")
-            return redirect(url_for("predict"))
+            return redirect(url_for("login"))
     else:
         return render_template("register.html")
 
--- db/clotDB.sql
+++ db/clotDB.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE clotDB;
 USE clotDB;
 CREATE TABLE `users` (
-  `email` varchar(256) NOT NULL,
-  `password` varchar(256) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(50) NOT NULL,
   PRIMARY KEY (`email`)
 );
 LOCK TABLES `users` WRITE;
 INSERT INTO `users`
-VALUES ('k@gmail.com', 'pbkdf2:sha256:260000$nRZddkrgdmjfCKjQ$507d15f4f51c202cc4a006127d4355ec7abef63e57fadcc9358f15289202810e'),
-  ('koushik.nov01@gmail.com', 'pbkdf2:sha256:260000$MwR0XcNAOMCvhMnO$dd9c5e1571ce7877237e8e6ec16dd198b224b4274385f633ba66ad3b06258a58'),
-  ('mahilogesh57@gmail.com', 'pbkdf2:sha256:260000$VU5jGxQlhSELyVDm$2ea674373cb7fa803df421265eaf1decbc9be9742fe4458deb5d1d0a0dcee516');
+VALUES ('k@gmail.com', 'k'),
+  ('koushik.nov01@gmail.com', 'koushik'),
+  ('mahilogesh57@gmail.com', 'logesh');
 UNLOCK TABLES;
